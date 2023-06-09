3317125f40084fc7ab8a84ef9aaa5ba467820669
Robert Kohr <Rob Kohr>
2023-06-09 14:47:23 -0700

    fix layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,12 +6,16 @@ import "./index.scss";
 export default function Home() {
   return (
     <div class="route-home" style="position:relative;top:-50px;">
-      <div class="header-image-section">
-        <h1>Robert Kohr Jr for Longview School Board</h1>
-        <h2>Bringing Charter Schools to Longview</h2>
-        <Donation />
+      <div class="header-image-section" style="display:flex; width:100%;align-content:stretch">
+        <div style=" flex-grow:4">
+          <h1>Robert Kohr Jr for Longview School Board</h1>
+          <h2>Bringing Charter Schools to Longview</h2>
 
-        <img src="/images/candidate.png" alt="candidate" class="candidate-head" />
+          <img src="/images/candidate.png" alt="candidate" class="candidate-head" />
+        </div>
+        <div>
+          <Donation />
+        </div>
       </div>
       <div class="content">
         <About />
